7d9149f94c301be5bf0b8fbda517b498ecd18fba
Hunain <272002abhishek@gmail.com>
2024-01-02 22:32:42 +0530

    set up the frontend

---
+++ frontend/src/Pages/Chatpage.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+import axios from 'axios';
+
+const Chatpage = () => {
+    const fetchChats = async()=> {
+        const data = await axios.get('/api/chat')
+    };
+    useEffect(()=>{
+        fetchChats();
+    }, [])
+    return (
+        <div>
+        Chats
+        </div>
+    )
+}
+
+export default Chatpage
